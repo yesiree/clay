c76b8e5c7a62fbc52d323aa54dddcd95df444779
Trevor <trevorhreed@gmail.com>
2018-09-11 17:00:03 -0600

    lots more updates

---
+++ cli.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+const getopts = require('getopts')
+const { getConfig, saveConfig } = require('./lib/config.js')
+
+const args = getopts(process.argv, {
+  boolean: ['init'],
+  default: {
+    init: false
+  }
+})
+
+const opts = {
+  init: args['_'].indexOf('init') !== -1,
+
+}
+
+if (opts.init) {
+  // create and save config object
+  const config = {}
+} else {
+  // run clay
+  const server = require('./lib/server.js')
+  const config = getConfig()
+  console.dir(config)
+}
+
+
